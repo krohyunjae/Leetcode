2c5083ebf92bf693f5c1f4d2f30bd7da878a7c75
krohyunjae <yroh3@wisc.edu>
2022-01-31 20:12:15 -0600

    Update Tests.java

--- leetCode/src/leetCode/Tests.java
+++ leetCode/src/leetCode/Tests.java
@@ -1,13 +1,7 @@
 package leetCode;
 
 import java.util.Arrays;
-class ListNode{
-	int val;
-	ListNode next;
-	ListNode(){}
-	ListNode(int val){this.val = val;}
-	ListNode (int val, ListNode next){this.val = val; this.next = next;}
-}
+
 
 public class Tests {
 	public void P13Test() {
