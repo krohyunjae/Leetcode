a728cee520c0268f2232e7e3f1d160e1f63df602
krohyunjae <yroh3@wisc.edu>
2024-02-07 08:52:06 -0600

    p0036 valid sodoku

--- LC/src/Leetcode/P0036.java
+++ LC/src/Leetcode/P0036.java
@@ -1,5 +1,7 @@
 package Leetcode;
 
+import java.util.*;
+
 public class P0036 {
     /**
      * Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
@@ -14,14 +16,33 @@ public class P0036 {
      */
 
     public boolean isValidSudoku(char[][] board) {
+        // Check if the row/column and each 3*3 cell contain the same number
+        boolean[][] col = new boolean[9][9];
+        boolean[][] row = new boolean[9][9];
+        boolean[][] box = new boolean[9][9];
 
-
-        return false;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '0' - 1;
+                    int k = (i/3) * 3 + j/3;
+                    if(row[i][num] || col[j][num] || box[k][num]) return false;
+                    row[i][num] = col[j][num] = box[k][num] = true;
+                }
+            }
+        }
+        return true;
     }
 
     public boolean isValidSequence(char[] sequence) {
-        for (int i = 0; i < ; i++) {
-
-        }
+        Set<Character> validSequence = new HashSet<>(Arrays.asList('1','2','3','4','5','6','7','8','9'));
+        if(Arrays.asList(sequence).containsAll(validSequence))
+            return true;
+        return false;
+//        for (int i = 0; i < 9; i++) {
+//            if(validSequence.contains(sequence[i])) {
+//                validSequence.
+//            }
+//        }
     }
 }
